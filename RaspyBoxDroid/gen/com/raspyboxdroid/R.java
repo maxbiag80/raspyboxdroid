3df04a92844b040c00d5e1066ebad598255dcdc4
maxbiag80 <m.biagioli@live.it>
2013-06-11 14:31:03 +0200

    Inserite preference

--- RaspyBoxDroid/gen/com/raspyboxdroid/R.java
+++ RaspyBoxDroid/gen/com/raspyboxdroid/R.java
@@ -17,40 +17,44 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
-        public static final int btnOffDevice1=0x7f080001;
-        public static final int btnOffDevice2=0x7f080004;
-        public static final int btnOffDevice3=0x7f080006;
-        public static final int btnOffDevice4=0x7f080008;
-        public static final int btnOnDevice1=0x7f080000;
-        public static final int btnOnDevice2=0x7f080003;
-        public static final int btnOnDevice3=0x7f080005;
-        public static final int btnOnDevice4=0x7f080007;
-        public static final int lblDevice1=0x7f080002;
-        public static final int lblDevice2=0x7f080009;
-        public static final int lblDevice3=0x7f08000a;
-        public static final int lblDevice4=0x7f08000b;
+        public static final int action_settings=0x7f09000c;
+        public static final int btnOffDevice1=0x7f090001;
+        public static final int btnOffDevice2=0x7f090004;
+        public static final int btnOffDevice3=0x7f090006;
+        public static final int btnOffDevice4=0x7f090008;
+        public static final int btnOnDevice1=0x7f090000;
+        public static final int btnOnDevice2=0x7f090003;
+        public static final int btnOnDevice3=0x7f090005;
+        public static final int btnOnDevice4=0x7f090007;
+        public static final int lblDevice1=0x7f090002;
+        public static final int lblDevice2=0x7f090009;
+        public static final int lblDevice3=0x7f09000a;
+        public static final int lblDevice4=0x7f09000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int Off=0x7f050005;
-        public static final int On=0x7f050004;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int device=0x7f050003;
-        public static final int hello_world=0x7f050002;
+        public static final int Off=0x7f060005;
+        public static final int On=0x7f060004;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int device=0x7f060003;
+        public static final int hello_world=0x7f060002;
+        public static final int ip_address=0x7f060008;
+        public static final int raspberrypi_info=0x7f060007;
+        public static final int summary_ip_address=0x7f060009;
+        public static final int title_activity_settings=0x7f060006;
     }
     public static final class style {
         /** 
@@ -74,10 +78,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int prefs=0x7f040000;
     }
 }
--- RaspyBoxDroid/src/com/raspyboxdroid/MainActivity.java
+++ RaspyBoxDroid/src/com/raspyboxdroid/MainActivity.java
@@ -1,8 +1,10 @@
 package com.raspyboxdroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
     
@@ -11,10 +13,23 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public boolean onCreateOptionsMenu(Menu menu) {        
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+		
+	public boolean onOptionsItemSelected(MenuItem item) {		
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			showSettings();
+			break;
+		}		
+		return super.onOptionsItemSelected(item);
+	}	
+	
+	private void showSettings() {
+		Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+		startActivity(intent);
+	}
+	
 }
