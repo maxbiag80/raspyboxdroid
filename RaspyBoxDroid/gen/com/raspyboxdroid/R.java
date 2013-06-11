aa5db9684ff13fc8113a1fb07be222517e952a1b
maxbiag80 <m.biagioli@live.it>
2013-06-11 13:52:13 +0200

    Disegnato layout

--- RaspyBoxDroid/gen/com/raspyboxdroid/R.java
+++ RaspyBoxDroid/gen/com/raspyboxdroid/R.java
@@ -24,7 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000c;
+        public static final int btnOffDevice1=0x7f080001;
+        public static final int btnOffDevice2=0x7f080004;
+        public static final int btnOffDevice3=0x7f080006;
+        public static final int btnOffDevice4=0x7f080008;
+        public static final int btnOnDevice1=0x7f080000;
+        public static final int btnOnDevice2=0x7f080003;
+        public static final int btnOnDevice3=0x7f080005;
+        public static final int btnOnDevice4=0x7f080007;
+        public static final int lblDevice1=0x7f080002;
+        public static final int lblDevice2=0x7f080009;
+        public static final int lblDevice3=0x7f08000a;
+        public static final int lblDevice4=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -33,8 +45,11 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int Off=0x7f050005;
+        public static final int On=0x7f050004;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int device=0x7f050003;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
